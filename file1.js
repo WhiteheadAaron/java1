6d0cdb1fe56deb1bb736c10ecaac63e263b936fb
Aaron Whitehead <whitehead.aaron@live.com>
2018-08-20 11:12:18 -0700

    added error message

--- file1.js
+++ file1.js
@@ -1,14 +1,28 @@
 function createGreeting(name, age) {
-    return `Hello my name is ${name} and I'm ${age} years old.`;
+    const birthyear = yearOfBirth(age);
+    return `Hello my name is ${name} and I'm ${age} years old. I was born in ${birthyear}`;
 }
 
 function yearOfBirth(age) {
     const year = 2018 - age;
-    return `I was born in ${year}`;
+
+    if (age < 0) {
+        throw new Error("Age cannot be negative");
+    }
+
+    else {
+        return year;
+    }
+}
+
+try {
+    yearOfBirth(2);
+}
+
+catch(e) {
+    console.error(e.message);
 }
 
 const output = createGreeting("Aaron", 24);
 console.log(output);
-const output2 = yearOfBirth(24);
-console.log(output2);
 
