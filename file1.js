b95616d3f8bdc8c36a1887ae0e0f35e6c25c9f6d
Aaron Whitehead <whitehead.aaron@live.com>
2018-08-20 11:03:12 -0700

    some changes

--- file1.js
+++ file1.js
@@ -1,8 +1,14 @@
 function createGreeting(name, age) {
-    const yearOfBirth = 2018 - age;
-    return `Hello my name is ${name} and I'm ${age} years old. I was born in ${yearOfBirth}`;
+    return `Hello my name is ${name} and I'm ${age} years old.`;
+}
+
+function yearOfBirth(age) {
+    const year = 2018 - age;
+    return `I was born in ${year}`;
 }
 
 const output = createGreeting("Aaron", 24);
 console.log(output);
+const output2 = yearOfBirth(24);
+console.log(output2);
 
