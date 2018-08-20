7bf0e99df1be0e0c2fbf14e01fdac05803be8b22
Aaron Whitehead <whitehead.aaron@live.com>
2018-08-20 11:15:41 -0700

    added another error message

--- file1.js
+++ file1.js
@@ -1,7 +1,20 @@
 function createGreeting(name, age) {
+
+    if (name === undefined || age === undefined) {
+        throw new Error("Arguments not valid");
+    }
     const birthyear = yearOfBirth(age);
     return `Hello my name is ${name} and I'm ${age} years old. I was born in ${birthyear}`;
 }
+
+try {
+    createGreeting("Aaron", 24);
+}
+catch(e) {
+    console.error(e.message);
+}
+
+
 
 function yearOfBirth(age) {
     const year = 2018 - age;
